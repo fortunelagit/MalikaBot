67ff7e5b89d2fec3d36cffc9b4aa70019efe3293
Khaela Fortunela <72397657+fortunelagit@users.noreply.github.com>
2021-07-07 11:30:50 +0700

    Create Weather.py

---
+++ Weather.py
@@ -0,0 +1,16 @@
+import Constants as keys
+
+
+def get_weather(city_name):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city_name, keys.WEATHER_API)
+    print(api_url)
+    response = requests.get(api_url)
+    response_dict = response.json()
+
+    weather = response_dict["weather"][0]["description"]
+
+    if response.status_code == 200:
+        return weather
+    else:
+        print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url))
+        return None
